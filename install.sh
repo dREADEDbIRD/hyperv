766e28eeb66c3debc374f638ab644dfeabd4d372
dreadedbird <legionelite6@gmail.com>
2024-06-04 17:57:31 +0700

    Initial commit.

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo pacman -Syyu fastfetch neovim stow zsh nodejs npm lua luarocks python xclip ttf-meslo-nerd ttf-sourcecodepro-nerd zsh-syntax-highlighting &
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions &
+
+curl -sS https://starship.rs/install.sh | sh &
+
+git clone https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim &
+
---
+++ xorg-setup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo pacman -Syyu xorg-server xorg-xinit libxft libxinerama xf86-video-fbdev xf86-video-intel arandr
